4cec8c7c0408e917c2c71ed43981ea01baab1b74
dustinanglin <dustin.anglin@gmail.com>
2020-02-26 01:56:25 -0800

    Photo Perf "Fix"
    If by "fix" I mean I have a really dumb workaround that's just dumb enough to work.

--- Assets/TakePicture.cs
+++ Assets/TakePicture.cs
@@ -6,10 +6,13 @@ public class TakePicture : MonoBehaviour
 {
     private bool screenshot;
     public GameObject Photospot;
+    //public GameObject CameraCanvas;
+    //private Renderer CanvasRenderer;
     // Start is called before the first frame update
     void Start()
     {
         screenshot = false;
+        //CanvasRenderer = CameraCanvas.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -29,7 +32,7 @@ public class TakePicture : MonoBehaviour
         {
             Debug.Log("Picture taken");
             screenshot = false;
-            RenderTexture.active = this.GetComponent<Camera>().targetTexture;
+            //RenderTexture.active = this.GetComponent<Camera>().targetTexture;
             RenderTexture renderTexture = RenderTexture.active;
             Texture2D texture = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGB24, false);
             //Read the pixels in the Rect starting at 0,0 and ending at the screen's width and height
