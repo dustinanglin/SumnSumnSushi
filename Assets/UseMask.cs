9ee4f64fda4e8e59464e296a6c68bd9a9b314373
dustinanglin <dustin.anglin@gmail.com>
2021-03-03 00:42:06 -0800

    Long time update

---
+++ Assets/UseMask.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseMask : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Renderer rend in GetComponentsInChildren<Renderer>())
+        {
+            rend.material.renderQueue = 2002;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
