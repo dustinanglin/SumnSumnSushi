3df34c2d8f40968204204baa82ceed4affdd08a0
dustinanglin <dustin.anglin@gmail.com>
2021-03-18 17:31:53 -0700

    Writing Tank article

--- Assets/TankAI.cs
+++ Assets/TankAI.cs
@@ -4,41 +4,114 @@ using UnityEngine;
 
 public class TankAI : MonoBehaviour
 {
-    public float gun_pitch_limit, aim_speed, swivel_speed;
-    private Transform AimPoint, Canon, Cab, Canon_initial, Cab_initial;
+    public float gun_pitch_limit, aim_speed, swivel_speed, fire_angle_threshold;
+    public bool aimAtPlayer, fireAtPlayer;
+    private float angleToTarget;
+    private Transform AimPoint, Canon, Turret, Canon_initial, Turret_initial, Canon_Fire;
+    private Quaternion initialTurretRotation, newTurretRotation, initialCanonRotation, newCanonRotation;
+
     // Start is called before the first frame update
     void Start()
     {
         AimPoint = GameObject.Find("Player/OVRCameraRig/TrackingSpace/CenterEyeAnchor/ShootTarget").transform;
 
-        Cab = transform.Find("Tank_Cab");
+        Turret = transform.Find("Tank_Cab");
         Canon = transform.Find("Tank_Cab/Gun_Swivel");
-        Canon_initial = Cab.Find("SwivelInitial");
-        Cab_initial = transform.Find("CabInitial");
+        Canon_initial = Turret.Find("SwivelInitial");
+        Turret_initial = transform.Find("CabInitial");
+        Canon_Fire = transform.Find("Tank_Cab/Gun_Swivel/Gun_Barrel/Bullet");
     }
 
     // Update is called once per frame
     void Update()
     {
-        AimAtGodzilla();
+        if (aimAtPlayer)
+            AimAtGodzilla();
+        if (fireAtPlayer)
+            FireAtGodzilla();
     }
 
     private void AimAtGodzilla()
     {
-        float pitch, yaw;
+        initialTurretRotation = Turret_initial.transform.rotation;
+        initialCanonRotation = Canon_initial.transform.rotation;
 
-        Vector3 toAimPointGun = AimPoint.position - Canon.position;
-        pitch = Mathf.Clamp(Vector3.SignedAngle(-Canon_initial.forward, Vector3.ProjectOnPlane(toAimPointGun, Canon_initial.right), Canon_initial.right), -gun_pitch_limit,gun_pitch_limit);
-        //Debug.Log(Vector3.SignedAngle(-Canon_initial.forward, Vector3.ProjectOnPlane(toAimPointGun, Canon_initial.right), Canon_initial.right));
- 
+        Vector3 AimVector;
 
-        Canon.rotation = Quaternion.Slerp(Canon.rotation,Canon_initial.rotation * Quaternion.AngleAxis(pitch, Vector3.right),aim_speed);
+        //********************************
+        //**** Rotate Turret Section *****
+        //********************************
 
-        Vector3 toAimPointCab = AimPoint.position - Cab.position;
-        yaw = Vector3.SignedAngle(-Cab_initial.forward, Vector3.ProjectOnPlane(toAimPointCab, Cab_initial.up), Cab_initial.up);
-        //Debug.Log("Cab Yaw:" + yaw);
-        Cab.rotation = Quaternion.Slerp(Cab.rotation,Cab_initial.rotation * Quaternion.AngleAxis(yaw, Vector3.up),swivel_speed);
+        AimVector = Turret.position - AimPoint.position; //Get a Vector from Turret to the Player
+        AimVector = Vector3.ProjectOnPlane(AimVector, Turret.up); //Project onto a "floor" plane so it only rotates side to sdie
+        newTurretRotation = Quaternion.LookRotation(AimVector, Turret.up); //Get a rotation using our projected vector
+
+        Quaternion q_Turret = newTurretRotation; //Temporary variable in case we want to do any kind of limits to the rotation
+        
+        Turret.rotation = Quaternion.Slerp(Turret.rotation, q_Turret, aim_speed * Time.deltaTime); //Rotate the turret with a smooth motion
+        angleToTarget = Quaternion.Angle(Turret.rotation, q_Turret); //Used to determine if the turret has finished rotating so it can fire
+
+        //********************************
+        //***** Swivel Canon Section *****
+        //********************************
+
+        AimVector = Canon.position - AimPoint.position; //Get a Vector from Turret to the Player
+        AimVector = Vector3.ProjectOnPlane(AimVector, Canon_initial.right); //Project onto a "side wall" plane so it only rotates up and down
+        newCanonRotation = Quaternion.LookRotation(AimVector, Canon_initial.up); //Get a rotation using our projected vector
 
 
+        Quaternion q_Canon = newCanonRotation; //Temporary variable so we can determine if it's hit the rotation limit
+
+        //Debug.Log("Euler angle: " + q_Canon.eulerAngles.x);
+
+        //If the angle between the current canon's rotation and the initial position
+        //has exceeded the angle limit, trying to aim at the player
+        if (Quaternion.Angle(initialCanonRotation, Canon.rotation) >= gun_pitch_limit)
+            q_Canon = Canon.rotation;
+
+        //If the angle between the current canon's rotation and the new spot it is aiming toward
+        //is less than the angle limit, go back to rotating
+        if (Quaternion.Angle(initialCanonRotation, newCanonRotation) < gun_pitch_limit)
+            q_Canon = newCanonRotation;
+
+        Canon.rotation = Quaternion.Slerp(Canon.rotation, q_Canon, swivel_speed * Time.deltaTime); //Rotate the canon with a smooth motion
     }
+
+    private void FireAtGodzilla()
+    {
+        //Debug.Log("Angle to Target: " + Mathf.FloorToInt(angleToTarget));
+
+        if (Mathf.FloorToInt(angleToTarget) == 0)
+        {
+            if (!Canon_Fire.gameObject.GetComponent<ParticleSystem>().isPlaying)
+                Canon_Fire.gameObject.GetComponent<ParticleSystem>().Play();
+        }
+        else
+        {
+            Canon_Fire.gameObject.GetComponent<ParticleSystem>().Stop();
+        }
+    }
+
+
+    // ******** LEGACY CODE FOR FUTURE ANTHROPOLOGISTS TO DISCOVER *********
+
+
+    //private void AimAtGodzilla()
+    //{
+    //    float pitch, yaw;
+
+    //    Vector3 toAimPointGun = AimPoint.position - Canon.position;
+    //    pitch = Mathf.Clamp(Vector3.SignedAngle(-Canon_initial.forward, Vector3.ProjectOnPlane(toAimPointGun, Canon_initial.right), Canon_initial.right), -gun_pitch_limit, gun_pitch_limit);
+    //    //Debug.Log(Vector3.SignedAngle(-Canon_initial.forward, Vector3.ProjectOnPlane(toAimPointGun, Canon_initial.right), Canon_initial.right));
+
+
+    //    Canon.rotation = Quaternion.Slerp(Canon.rotation, Canon_initial.rotation * Quaternion.AngleAxis(pitch, Vector3.right), aim_speed);
+
+    //    Vector3 toAimPointCab = AimPoint.position - Cab.position;
+    //    yaw = Vector3.SignedAngle(-Cab_initial.forward, Vector3.ProjectOnPlane(toAimPointCab, Cab_initial.up), Cab_initial.up);
+    //    //Debug.Log("Cab Yaw:" + yaw);
+    //    Cab.rotation = Quaternion.Slerp(Cab.rotation, Cab_initial.rotation * Quaternion.AngleAxis(yaw, Vector3.up), swivel_speed);
+
+    //}
+
 }
--- Assets/TextFacePlayer.cs
+++ Assets/TextFacePlayer.cs
@@ -6,11 +6,13 @@ public class TextFacePlayer : MonoBehaviour
 {
     private GameObject head;
     public bool facePlayer;
+    private Quaternion newRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         head = GameObject.Find("CenterEyeAnchor");
+
     }
 
     // Update is called once per frame
@@ -19,8 +21,9 @@ public class TextFacePlayer : MonoBehaviour
         if (facePlayer)
         {
             Vector3 lookAtHead = this.transform.position - head.transform.position; //reverse because Text is funky pants
-            lookAtHead = Vector3.ProjectOnPlane(lookAtHead, Vector3.up); //project onto a "floor" plane so it only rotates around the Y axis
-            this.transform.rotation = Quaternion.LookRotation(lookAtHead, Vector3.up);
+            lookAtHead = Vector3.ProjectOnPlane(lookAtHead, transform.up); //project onto a "floor" plane so it only rotates around the Y axis
+            newRotation = Quaternion.LookRotation(lookAtHead, transform.up);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, newRotation, 0.01f);
         }
     }
 }
