0d2a31e70b4b3148b700c9d3c2ab201d4c555e7b
dustinanglin <dustin.anglin@gmail.com>
2021-04-15 03:00:07 -0700

    Saber So Pretty

--- Assets/SaberInteraction.cs
+++ Assets/SaberInteraction.cs
@@ -1,15 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using MeshSplitting.Examples;
+using MeshSplitting.Splitables;
 using UnityEngine;
 
 public class SaberInteraction : MonoBehaviour
 {
     public GameObject startSphere, endSphere, velocityTracker;
 
+    [SerializeField]
+    private float bladeOffset1, bladeOffset2, bladeCapOffset;
+
+    [SerializeField]
+    private float bladeGlowSpeed;
+
+    [SerializeField]
+    private float bladeGlowVariance;
+
+    [SerializeField]
+    private float bladeWidth;
+
+    [SerializeField]
+    private float bladeDropOff;
+
+    private GameObject sparks, sparksBoom;
+
     private Vector3 startPoint, endPoint, velocity;
-    private GameObject cutter;
+    private GameObject cutter, sparkCaster;
     private Rigidbody saberRB;
+    private LineRenderer bladeGlow, bladeCap;
 
     private bool doingCut = false;
 
@@ -18,6 +37,11 @@ public class SaberInteraction : MonoBehaviour
     {
         cutter = GameObject.Find("CenterEyeAnchor");
         saberRB = velocityTracker.GetComponent<Rigidbody>();
+        bladeGlow = GetComponent<LineRenderer>();
+        bladeCap = transform.Find("BladeCap").gameObject.GetComponent<LineRenderer>();
+        sparks = transform.Find("SparkFount").gameObject;
+        sparksBoom = transform.Find("SparkFountBoom").gameObject;
+        sparkCaster = transform.Find("SparkCaster").gameObject;
     }
 
     // Update is called once per frame
@@ -31,6 +55,24 @@ public class SaberInteraction : MonoBehaviour
         Debug.DrawRay(transform.position, transform.up, Color.blue);
         if (Vector3.Magnitude(_tempVelocity) >= .02f)
             Debug.DrawRay(transform.position, velocity, Color.red);
+
+        DrawBlade();
+    }
+
+
+    private void DrawBlade()
+    {
+        bladeGlow.SetPosition(2, transform.position + transform.rotation * new Vector3(0, bladeOffset1, 0));
+        bladeGlow.SetPosition(1, transform.position + transform.rotation * new Vector3(0, -bladeOffset2 + bladeDropOff, 0));
+        bladeGlow.SetPosition(0, transform.position + transform.rotation * new Vector3(0, -bladeOffset2 , 0));
+
+        bladeCap.SetPosition(0, transform.position + transform.rotation * new Vector3(0, bladeOffset1, 0));
+        bladeCap.SetPosition(1, transform.position + transform.rotation * new Vector3(0, bladeOffset1 + bladeCapOffset, 0));
+
+        bladeGlow.startWidth = bladeWidth + Mathf.Sin(Time.time * bladeGlowSpeed) * bladeGlowVariance;
+        bladeGlow.endWidth = bladeWidth + Mathf.Sin(Time.time * bladeGlowSpeed) * bladeGlowVariance;
+        bladeCap.startWidth = bladeWidth + Mathf.Sin(Time.time * bladeGlowSpeed) * bladeGlowVariance;
+        bladeCap.endWidth = bladeWidth + Mathf.Sin(Time.time * bladeGlowSpeed) * bladeGlowVariance;
     }
 
     /*void FixedUpdate()
@@ -42,32 +84,60 @@ public class SaberInteraction : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Collide!");
 
-
-        collision.gameObject.GetComponent<Rigidbody>().isKinematic = false;
-        Debug.Log("Colliding point: " + collision.GetContact(0).point);
         startSphere.transform.position = collision.GetContact(0).point;
         startPoint = collision.GetContact(0).point;
         endPoint = transform.position + velocity;
         endSphere.transform.position = endPoint;
 
-        cutter.GetComponent<CameraLineSplitter>().doColliderCut(startPoint, endPoint, transform.up);
-
-        if (collision.gameObject.GetComponent<CutoutMotor>())
+        if (collision.gameObject.GetComponent<Splitable>())
         {
-            collision.gameObject.GetComponent<CutoutMotor>().SetSpeed(0);
-            collision.gameObject.GetComponent<CutoutMotor>().MotorOff();
-            collision.gameObject.GetComponent<CutoutMotor>().SetDeathTimer();
+            collision.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            Debug.Log("Colliding point: " + collision.GetContact(0).point);
+            cutter.GetComponent<CameraLineSplitter>().doColliderCut(startPoint, endPoint, transform.up);
 
+            if (collision.gameObject.GetComponent<CutoutMotor>())
+            {
+                collision.gameObject.GetComponent<CutoutMotor>().SetSpeed(0);
+                collision.gameObject.GetComponent<CutoutMotor>().MotorOff();
+                collision.gameObject.GetComponent<CutoutMotor>().SetDeathTimer();
+            }
+
+            setSparkSpot(sparksBoom, collision.GetContact(0));
+            sparksBoom.GetComponent<ParticleSystem>().Play();
         }
-    }         
+        else
+        {
+            setSparkSpot(sparks);
+            sparks.SetActive(true);
+        }
+
+    }
+    
+    private void setSparkSpot(GameObject sp)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(sparkCaster.transform.position, transform.up);
+        Physics.Raycast(sparkCaster.transform.position, transform.up,out hit,1.0f);
+        sp.transform.position = hit.point;
+        endSphere.transform.position = hit.point;
+        sp.transform.rotation = Quaternion.LookRotation(hit.normal);
+    }
+
+    private void setSparkSpot(GameObject sp, ContactPoint point)
+    {
+        sp.transform.position = point.point;
+        sp.transform.rotation = Quaternion.LookRotation(point.normal);
+    }
 
     private void OnCollisionStay(Collision collision)
     {
-        //endPoint = collision.GetContact(collision.contactCount - 1).point;
+        //endPoint = collision.GetContact(0).point;
         //endSphere.transform.position = endPoint;
         //if (collision.gameObject.GetComponent<CutoutMotor>())
-            //collision.gameObject.GetComponent<CutoutMotor>().SetSpeed(2);
+        //collision.gameObject.GetComponent<CutoutMotor>().SetSpeed(2);
+        setSparkSpot(sparks);
     }
 
     private void OnCollisionExit(Collision collision)
@@ -82,7 +152,7 @@ public class SaberInteraction : MonoBehaviour
         }
         cutter.GetComponent<CameraLineSplitter>().doColliderCut(startPoint, endPoint,transform.up);*/
 
-
+        sparks.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
